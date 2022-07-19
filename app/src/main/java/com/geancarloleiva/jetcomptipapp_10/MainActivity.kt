6ff6bbfe0cff1149e1f825157ddf3c722675c71d
geanclv <geanclv@gmail.com>
2022-07-19 17:40:10 -0500

    Creating the UI header.

--- app/src/main/java/com/geancarloleiva/jetcomptipapp_10/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/jetcomptipapp_10/MainActivity.kt
@@ -3,41 +3,73 @@ package com.geancarloleiva.jetcomptipapp_10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.geancarloleiva.jetcomptipapp_10.ui.theme.JetCompTipApp_10Theme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetCompTipApp_10Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                CreateMainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateMainScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color.White
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            CreateHeader()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetCompTipApp_10Theme {
-        Greeting("Android")
+fun CreateHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color.LightGray
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%,.2f".format(totalPerPerson)
+            Text(
+                text = "Total per person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$${total}",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+
     }
 }
