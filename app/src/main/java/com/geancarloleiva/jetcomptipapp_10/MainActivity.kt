9ff6689780a6fd06bc69260095829f483032b497
geanclv <geanclv@gmail.com>
2022-07-20 11:49:59 -0500

    Finishing the UI. Adding the logic to calculate the bill amount per person.

--- app/src/main/java/com/geancarloleiva/jetcomptipapp_10/MainActivity.kt
+++ app/src/main/java/com/geancarloleiva/jetcomptipapp_10/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.geancarloleiva.jetcomptipapp_10.component.InputField
 import com.geancarloleiva.jetcomptipapp_10.ui.theme.JetCompTipApp_10Theme
@@ -55,14 +55,13 @@ fun CreateMainScreen() {
         color = Color.White
     ) {
         Column(modifier = Modifier.padding(10.dp)) {
-            CreateHeader()
             CreateBody()
         }
     }
 }
 
 @Composable
-fun CreateHeader(totalPerPerson: Double = 0.0) {
+fun CreateBillPerPersonSpace(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -92,32 +91,51 @@ fun CreateHeader(totalPerPerson: Double = 0.0) {
 
 @Composable
 fun CreateBody() {
-    CreateBillForm() { billAmount ->
-        //TODO billAmount will be divided between number of persons and then showed in header
+    val stateTotalPerPerson = remember {
+        mutableStateOf(0.0)
+    }
+    val stateTotalBillAmount = remember {
+        mutableStateOf("")
+    }
+    val stateTotalPerson = remember {
+        mutableStateOf(1)
+    }
+    val stateTotalTip = remember {
+        mutableStateOf(0f)
+    }
+
+    CreateBillForm(stateTotalPerPerson = stateTotalPerPerson,
+        stateTotalBillAmount = stateTotalBillAmount,
+        stateTotalPerson = stateTotalPerson,
+        stateTotalTip = stateTotalTip) { billAmount ->
+        Log.d("Test", "Bill amount: $billAmount")
     }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
-@Preview
 @Composable
 fun CreateBillForm(
     modifier: Modifier = Modifier,
+    stateTotalPerPerson: MutableState<Double>,
+    stateTotalBillAmount: MutableState<String>,
+    stateTotalPerson: MutableState<Int>,
+    stateTotalTip: MutableState<Float>,
     onValueChange: (String) -> Unit = {}
 ) {
     val context = LocalContext.current
-    val totalBillAmountState = remember {
-        mutableStateOf("")
-    }
-    val validState = remember(totalBillAmountState.value) {
-        totalBillAmountState.value.trim().isNotEmpty()
-    }
     val keyboardController = LocalSoftwareKeyboardController.current
-    val totalPerson = remember {
-        mutableStateOf(0)
+
+    val stateValid = remember(stateTotalBillAmount.value) {
+        stateTotalBillAmount.value.trim().isNotEmpty()
+    }
+    val stateSliderValue = remember {
+        mutableStateOf(0f)
     }
 
+    CreateBillPerPersonSpace(stateTotalPerPerson.value)
+
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .padding(2.dp)
             .fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
@@ -128,30 +146,32 @@ fun CreateBillForm(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
+            //Bill amount
             InputField(modifier = Modifier.fillMaxWidth(),
-                valueState = totalBillAmountState,
+                valueState = stateTotalBillAmount,
                 labelId = "Bill amount",
                 isEnabled = true,
                 isSingleLine = true,
                 onAction = KeyboardActions {
-                    if (!validState) {
+                    if (!stateValid) {
                         return@KeyboardActions
                     }
-                    onValueChange(totalBillAmountState.value.trim())
+                    onValueChange(stateTotalBillAmount.value.trim())
                     keyboardController?.hide()
                 }
             )
 
-            if (validState) {
+            if (stateValid) {
+                //Persons Row
                 Row(
                     modifier = Modifier.padding(3.dp),
                     horizontalArrangement = Arrangement.Start
                 ) {
                     Text(
-                        text = "Split:",
+                        text = "Nr. of persons:",
                         modifier = Modifier.align(alignment = Alignment.CenterVertically)
                     )
-                    Spacer(modifier = Modifier.width(120.dp))
+                    Spacer(modifier = Modifier.width(70.dp))
                     Row(
                         modifier = Modifier.padding(horizontal = 3.dp),
                         horizontalArrangement = Arrangement.End
@@ -159,19 +179,24 @@ fun CreateBillForm(
                         RoundIconButton(
                             imageVector = Icons.Default.Remove,
                             onClick = {
-                                if (totalPerson.value == 0) {
+                                if (stateTotalPerson.value == 1) {
                                     Toast.makeText(
                                         context, "You can remove more person",
                                         Toast.LENGTH_SHORT
                                     ).show()
                                 } else {
-                                    totalPerson.value--
+                                    stateTotalPerson.value--
+
+                                    stateTotalPerPerson.value = calculateTotalPerPerson(
+                                        stateTotalBillAmount.value.toFloat(),
+                                        stateTotalTip.value, stateTotalPerson.value
+                                    )
                                 }
                             },
                             buttonDescription = "Remove"
                         )
                         Text(
-                            text = totalPerson.value.toString(),
+                            text = stateTotalPerson.value.toString(),
                             modifier = Modifier
                                 .align(Alignment.CenterVertically)
                                 .padding(start = 9.dp, end = 9.dp)
@@ -179,15 +204,103 @@ fun CreateBillForm(
                         RoundIconButton(
                             imageVector = Icons.Default.Add,
                             onClick = {
-                                totalPerson.value++
+                                stateTotalPerson.value++
+
+                                stateTotalPerPerson.value = calculateTotalPerPerson(
+                                    stateTotalBillAmount.value.toFloat(),
+                                    stateTotalTip.value, stateTotalPerson.value
+                                )
                             },
                             buttonDescription = "Add"
                         )
                     }
                 }
+
+                //Tip Row
+                Row(
+                    modifier = Modifier.padding(
+                        horizontal = 3.dp,
+                        vertical = 12.dp
+                    ),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Total tip:",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(140.dp))
+                    Text(
+                        text = "$ ${stateTotalTip.value}",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                }
+
+                //Tip % Row
+                Row(
+                    modifier = Modifier.padding(
+                        horizontal = 3.dp,
+                        vertical = 12.dp
+                    ),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "% of tip:",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(170.dp))
+                    Text(
+                        text = "${stateSliderValue.value.toInt()} %",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                }
+
+                //Slider
+                Slider(
+                    value = stateSliderValue.value,
+                    onValueChange = { newSliderValue ->
+                        stateSliderValue.value = newSliderValue
+                        stateTotalTip.value = calculateTotalTip(
+                            stateTotalBillAmount.value.toFloat(), stateSliderValue.value.toInt()
+                        )
+
+                        stateTotalPerPerson.value = calculateTotalPerPerson(
+                            stateTotalBillAmount.value.toFloat(),
+                            stateTotalTip.value, stateTotalPerson.value
+                        )
+                    },
+                    valueRange = 0f..100f,
+                    onValueChangeFinished = {
+
+                    }
+                )
             } else {
 
             }
         }
     }
+}
+
+fun calculateTotalTip(
+    billAmount: Float,
+    tipPercentage: Int
+): Float {
+    var totalTip = 0f
+    if (billAmount > 1 && billAmount.toString().isNotEmpty()) {
+        totalTip = (billAmount * tipPercentage) / 100
+    }
+    return totalTip
+}
+
+fun calculateTotalPerPerson(
+    billAmount: Float,
+    totalTip: Float,
+    nbrPersons: Int
+): Double {
+    var totalPerPerson = 0.0
+    if (billAmount > 1 && billAmount.toString().isNotEmpty()
+        && totalTip > 0 && totalTip.toString().isNotEmpty()
+    ) {
+        totalPerPerson = ((billAmount + totalTip) / nbrPersons).toDouble()
+    }
+    return totalPerPerson
 }
